f811e4457b8524c35de3faa0ec7ba4753df8182d
Michal Voců <michal.vocu@gmail.com>
2014-02-03 10:09:23 +0100

    do not insert test tasks into the database

--- perun-engine/src/test/resources/test-data.sql
+++ perun-engine/src/test/resources/test-data.sql
@@ -16,12 +16,6 @@ values (0, 'testFacility', 'host');
 insert into engines (id, ip_address, port)
 values (0, '127.0.0.1', 5560);
 
-insert into tasks (id, exec_service_id, facility_id, schedule, recurrence, delay, status, engine_id)
-values (1, 1, 0, now, 5, 5, 'NONE', 0);
-
-insert into tasks (id, exec_service_id, facility_id, schedule, recurrence, delay, status, engine_id)
-values (2, 2, 0, now, 5, 5, 'NONE', 0);
-
 insert into destinations (id, destination, type) 
 values (1, 'par_dest1', 'PARALLEL');
 
@@ -33,3 +27,4 @@ values (3, 'par_dest3', 'ONE');
 
 insert into destinations (id, destination, type) 
 values (4, 'par_dest4', 'ONE');
+
